01fe5dbf9f88002c5d6c54753eb766862244ffdd
Bohdan <nalivkin.bog@gmail.com>
2023-01-16 16:25:42 +0200

    final fix

--- src/js/index.js
+++ src/js/index.js
@@ -45,20 +45,24 @@ btnLoadMore.addEventListener('click', () => {
   const trimmedValue = input.value.trim();
   btnLoadMore.style.display = 'none';
   fetchImages(trimmedValue, pageNumber).then(foundData => {
-    if (foundData.hits.length === 0) {
-      Notiflix.Notify.failure(
-        'Sorry, there are no images matching your search query. Please try again.'
-      );
-    } else {
+    // if (foundData.hits.length === 0) {
+    //   Notiflix.Notify.failure(
+    //     'Sorry, there are no images matching your search query. Please try again.'
+    //   );
+    // } else {
       renderImageList(foundData.hits);
-      Notiflix.Notify.success(
-        `Hooray! We found ${foundData.totalHits} images.`
-      );
-      btnLoadMore.style.display = 'block';
+    //   Notiflix.Notify.success(
+    //     `Hooray! We found ${foundData.totalHits} images.`
+    //   );
+    //   btnLoadMore.style.display = 'block';
+    //   if (40 * pageNumber >= foundData.totalHits) {
+    //     btnLoadMore.style.display = 'none';
+    //   }
+    // }
+    btnLoadMore.style.display = 'block';
       if (40 * pageNumber >= foundData.totalHits) {
         btnLoadMore.style.display = 'none';
       }
-    }
   });
 });
 
